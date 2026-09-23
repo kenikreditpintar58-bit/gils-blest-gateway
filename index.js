179c206955020b337a971b34bcf90f4ae4dce74b
kenikreditpintar58-bit <kenikreditpintar58@gmail.com>
2026-09-23 08:51:03 +0700

    Update index.js

--- index.js
+++ index.js
@@ -83,7 +83,7 @@ app.get('/', (req, res) => {
     res.send('Gils Blest Multi-Device Railway Gateway is Running Online.');
 });
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, '0.0.0.0', () => {
-    console.log(`Gateway berjalan di port ${PORT}`);
+    console.log(`Server berjalan di port ${PORT}`);
 });
